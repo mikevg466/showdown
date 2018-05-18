5c96e71233647a261f9fb96b62765a3cbe1381c9
Michael van Genderen <mikevg466@gmail.com>
2018-05-17 23:42:48 -0400

    style: Updates the links to the brackets and hotOrNot pages to be image icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,5 +1,7 @@
 export { default as Apple } from './Apple';
 export { default as Boxing } from './Boxing';
+export { default as Brackets } from './Brackets';
+export { default as Fire } from './Fire';
 export { default as Orange } from './Orange';
 export { default as Robot } from './Robot';
 export { default as Winner } from './Winner';
--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Brackets, Fire } from '../Icons';
 import './Links.css';
 
 const Links = props => (
   <div className="links">
-    <Link to="/brackets">Brackets</Link>
-    <Link to="/hotornot">Hot Or Not</Link>
+    <div className="page-link">
+      <h1>Brackets</h1>
+      <Link to="/brackets">
+        <Brackets />
+      </Link>
+    </div>
+    <div className="divider">
+    </div>
+    <div className="page-link">
+      <h1>Hot Or Not</h1>
+      <Link to="/hotornot">
+        <Fire />
+      </Link>
+    </div>
   </div>
 );
 
